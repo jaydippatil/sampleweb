e5c76465aab382457d815f3aab5ed854c0350941
Jaydip Patil <jaydippatil.com@gmail.com>
2020-05-16 00:26:24 +0530

    Firstweb

---
+++ SampleWebapp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SampleWebapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
